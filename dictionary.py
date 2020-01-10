57bb22753e23680559f686fc620951887ac1d1f2
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-01-10 23:17:55 +0900

    2020.01.10

--- dictionary.py
+++ dictionary.py
@@ -3,11 +3,18 @@ dict = {}
 dict['안녕'] = 'Hello'
 dict['기적'] = 'Miracle'
 dict['노력'] = 'effort'
+# 덧쓰기
 dict['안녕'] = 'Hi'
 print(dict)
 # 제거
 del dict['기적']
+# dictionary 전체 삭제
 dict.clear()
+
+# 사전 자료형의 길이
 print("사전 자료형의 길이", len(dict))
 #for i, k in enumerate(dict):
  #  print("[인덱스 :", i, "]한글 :", k , "영어 :" ,dict[k])
+
+for i , k in enumerate(dict):
+    print("")
---
+++ practice.py
@@ -0,0 +1,7 @@
+
+today = ['a', 'b', 'c']
+for z in today:
+    print(z)
+
+for i in range(0,10):
+    print(i)
